3a0c199f63faf260bf18e2cdbd5eb76edd8d4f4a
Alexdar <AlexDar@angelsit.ru>
2021-10-14 19:33:21 +0300

    Попытка сделать html app.add_url_rule не работает и html не выводится

--- views.py
+++ views.py
@@ -2,24 +2,26 @@ from flask import jsonify
 from main import app
 from models import Locality
 from models import Ticket
-from datetime import datetime
+from flask.views import View
 
 
-@app.route('/')
-@app.route('/home')
 def home():
-    title = "Главная страница"
-    source = """<html><body>
-            <h1>{{h1}}</h1>
-            <h3><a href="{{url_for('hello')}}">Страница с приветом...</a></h3>
-            </body></html>"""
-    return jsonify(source, h1=title)
-    # return jsonify(
-    #     'index.html',
-    #     title='Home Page',
-    #     year=datetime.now().year,
-    # )
+    return jsonify('index.html', context='Это главная страница')
 
+
+app.add_url_rule('/', view_func=home)
+app.add_url_rule('/home', view_func=home)
+
+
+class Showlocalitys(View):
+
+    def dispatch_request(self):
+        localitys = Locality.query.all()
+        return jsonify([l.to_dict() for l in localities])
+        #return jsonify('users.html', objects=localitys)
+
+
+app.add_url_rule('/locality/', view_func=Showlocalitys.as_view('show_localitys'))
 
 @app.route("/locality")
 def get_locality():
@@ -31,4 +33,21 @@ def get_locality():
 def get_Tickets():
     Tickets = Ticket.query.all()
     return jsonify([l.to_dict() for l in Tickets])
+
+
+# @app.route('/')
+# @app.route('/home')
+#def home():
+# title = "Главная страница"
+    #source = """<html><body>
+    #        <h1>{{h1}}</h1>
+    #        <h3><a href="{{url_for('hello')}}">Страница с приветом...</a></h3>
+    #        </body></html>"""
+    # return jsonify(source, h1=title)
+    # return jsonify(
+    #     'index.html',
+    #     title='Home Page',
+    #     year=datetime.now().year,
+    # )
+
 
