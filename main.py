7ffe504a7173e11f9853ed394ffed3ae5d5119a8
Дарья Добрынина <91492798+AlexDar1@users.noreply.github.com>
2021-11-05 17:04:47 +0300

    Вернулась к SQLLight

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://tickets:postgres@db:5432/tickets"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///test.db"
+#app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://tickets:postgres@localhost:5432/tickets"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -13,4 +14,4 @@ from views import *
 from models import *
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="127.0.0.1", port=8000)
